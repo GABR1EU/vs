59bdb3e6e7d33d5d61ecd4c4211c80447360ea95
GABR1EU <martinsgabriel489@gmail.com>
2026-05-21 23:12:47 -0300

    Update get-views.js

--- api/get-views.js
+++ api/get-views.js
@@ -2,7 +2,7 @@
 
 const CONFIG = {
     principais: ['j-rlc0o6ILA', 'aP_1McdVF_4', 'OHZHCgU9Y9g', '9kdYCYZtom8', 'CFrrpVBsTWU', 'oyTlY_aK7W0', 'Y5k9pbT9ZQQ', 'LF1U8TZUSx4'],
-    secundarios: ['9c3wOQRGR30', 'l0mNft1jFjI', 'P1aI_WmkqAY', 'XoVvt8Ws12U', '93PezDiU5vc', 'APpxoX_Kueo', 'wDe4g28vog8', 'saWLga8_y_Y'],
+    secundarios: ['9c3wOQRGR30', 'l0mNft1jFjI', 'P1aI_WmkqAY', 'XoVvt8Ws12U', '93PezDiU5vc', 'APpxoX_Kueo', 'wDe4g28vog8', 'saWLga8_y_Y', 'F6qfUP1tLow'],
     ambos: ['NrUluayuOK4', 'YC5jGimSeNo', 'Q72h8lVbNaE']
 };
 
